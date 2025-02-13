f623d5fb273901d9def7be0ef615b09e74fb9677
ValerioDev6 <vquispealarcon@gmail.com>
2025-02-13 10:07:30 -0500

    Nest bases typeorm conexcion postrgress

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { UsersModule } from '@modules/users/users.module';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+      isGlobal: true,
+    }),
+    UsersModule,
+  ],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { CORS } from '@constants/cors';
+
+async function bootstrap() {
+  // const app = await NestFactory.create(AppModule);
+
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log'],
+  });
+
+  const configService = app.get(ConfigService);
+  app.enableCors(CORS);
+  app.setGlobalPrefix('v1/api');
+  const port = configService.get('PORT') || 3000;
+
+  await app.listen(port);
+  console.log(`🚀 Application is running on: http://localhost:${port}`);
+}
+bootstrap();
